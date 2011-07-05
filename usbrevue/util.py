5b1878340a31747af200060b1e6fba4dc0b37336
Wil Cooley <wcooley@pdx.edu>
2011-07-05 00:02:16 -0700

    Fix reversed variables; add doctest docs

--- usbrevue/util.py
+++ usbrevue/util.py
@@ -3,5 +3,16 @@
 """Miscellaneous utilities"""
 
 def reverse_update_dict(dictionary):
-    """Update dictionary by adding val => key"""
-    dictionary.update([ (val,key) for val,key in dictionary.items() ])
+    """Update dictionary by adding val => key
+
+    >>> d = dict(a=1)
+    >>> reverse_update_dict(d)
+    >>> d
+    {'a': 1, 1: 'a'}
+    """
+    dictionary.update([ (val,key) for key,val in dictionary.items() ])
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
