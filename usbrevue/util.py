d6c764cf9f6e346811182e1b5f446761b86b6ea0
Wil Cooley <wcooley@pdx.edu>
2011-07-08 14:50:00 -0700

    Added apply_mask function

--- usbrevue/util.py
+++ usbrevue/util.py
@@ -12,6 +12,20 @@ def reverse_update_dict(dictionary):
     """
     dictionary.update([ (val,key) for key,val in dictionary.items() ])
 
+def apply_mask(mask, oval, nval):
+    """Apply a mask with nval to oval, without disturbing bits in ~mask.
+
+    >>> bin(apply_mask(0b11000000, 0b11000000, 0b11000000))
+    '0b11000000'
+    >>> bin(apply_mask(0b11000000, 0b01010101, 0b10000010))
+    '0b10010101'
+    >>> bin(apply_mask(0b11001100, 0b11111111, 0b00101000))
+    '0b111011'
+    >>> bin(apply_mask(0b11001100, 0b00000000, 0b00101000))
+    '0b1000'
+    """
+    return ((mask & nval) | ( ~mask & oval ))
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
