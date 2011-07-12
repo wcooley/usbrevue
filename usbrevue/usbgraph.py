9489569030899504727e6171a359e4fe0f62b0f5
Austin Leirvik <austin.leirvik@gmail.com>
2011-07-11 19:31:09 -0700

    table and plot widget are reading from a global list variable -- not a good implementation

--- usbrevue/usbgraph.py
+++ usbrevue/usbgraph.py
@@ -5,8 +5,58 @@ from usbview import PcapThread
 from usbrevue import Packet
 from PyQt4 import Qt
 from PyQt4.QtGui import *
-from PyQt4.QtCore import QAbstractTableModel, QModelIndex, QVariant
+from PyQt4.QtCore import QAbstractTableModel, QModelIndex, QVariant, QString, QByteArray
 import PyQt4.Qwt5 as Qwt
+
+
+class ByteModel(QAbstractTableModel):
+    def __init__(self, parent=None):
+        QAbstractTableModel.__init__(self, parent)
+
+
+    def rowCount(self, parent = QModelIndex()):
+        return 100
+
+    def columnCount(self, parent = QModelIndex()):
+        return 4
+
+    def data(self, index, role = Qt.Qt.DisplayRole):
+        row = index.row()
+        col = index.column()
+
+        if (role == Qt.Qt.DisplayRole):
+            if len(bytes) > col and len(bytes[col]) > row:
+                return QVariant(bytes[col][row])
+                # return QString('Row%1, Column%2').arg(index.row() + 1).arg(index.column() + 1)
+        return QVariant()
+
+    def headerData(self, section, orientation, role = Qt.Qt.DisplayRole):
+        if (role == Qt.Qt.DisplayRole):
+            if (orientation == Qt.Qt.Horizontal):
+                if section == 0:
+                    return QString("first")
+                elif section == 1:
+                    return QString("second")
+                elif section == 2:
+                    return QString("third")
+        return QVariant()
+
+
+
+
+class ByteView(QTableView):
+    def __init__(self, parent=None):
+        QTableView.__init__(self, parent)
+
+    def new_packet(self, packet):
+        if len(packet.data) > 0:
+            if len(bytes) < len(packet.data):
+                for i in range(len(packet.data) - len(bytes)):
+                    bytes.append(list())
+                    
+            for i in range(len(packet.data)):
+                bytes[i].append(packet.data[i])
+
 
 
 class BytePlot(Qwt.QwtPlot):
@@ -17,11 +67,14 @@ class BytePlot(Qwt.QwtPlot):
         self.alignScales()
 
         self.x = range(100)
-        self.bytes = list()
-
+        
         self.byteCurves = list()
 
         self.insertLegend(Qwt.QwtLegend(), Qwt.QwtPlot.BottomLegend)
+
+
+    def setModel(self, model):
+        self.model = model
 
 
     def alignScales(self):
@@ -39,16 +92,12 @@ class BytePlot(Qwt.QwtPlot):
 
     def new_packet(self, packet):
         if len(packet.data) > 0:
-            if len(self.bytes) < len(packet.data):
-                for i in range(len(packet.data) - len(self.bytes)):
-                    self.bytes.append(list())
+            if len(self.byteCurves) < len(packet.data):
+                for i in range(len(packet.data) - len(self.byteCurves)):
                     self.add_byte_curve(i)
 
-            for i in range(len(packet.data)):
-                self.bytes[i].append(packet.data[i])
-
-            for i in range(len(self.bytes)):
-                self.byteCurves[i].setData(self.x, self.bytes[i])
+            for i in range(len(bytes)):
+                self.byteCurves[i].setData(self.x, bytes[i])
 
             self.replot()
 
@@ -66,9 +115,14 @@ class USBGraph(QApplication):
         self.w = QWidget()
         self.w.resize(800, 600)
 
+        self.bytemodel = ByteModel()
+        self.byteview = ByteView()
+        self.byteview.setModel(self.bytemodel)
         self.byteplot = BytePlot()
+        self.byteplot.setModel(self.bytemodel)
 
         self.hb = QHBoxLayout()
+        self.hb.addWidget(self.byteview)
         self.hb.addWidget(self.byteplot)
 
         self.w.setLayout(self.hb)
@@ -78,7 +132,6 @@ class USBGraph(QApplication):
         self.pcapthread.dump_opened.connect(self.dump_opened)
         self.pcapthread.new_packet.connect(self.new_packet)
         self.pcapthread.start()
-
         self.dumper = None
 
 
@@ -87,8 +140,11 @@ class USBGraph(QApplication):
 
 
     def new_packet(self, packet):
-        self.byteplot.new_packet(packet)
-        self.bytemodel.new_packet(packet)
+        self.byteview.new_packet(packet)
+
+
+
+bytes = list()
 
 
 if __name__ == "__main__":
