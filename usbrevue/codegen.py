8c8d3936e8ea6d017ff0bb82260bed572b49e181
dw aley <danny.aley@gmail.com>
2011-07-18 03:10:12 -0700

    codegen: imaginary error handling

--- usbrevue/codegen.py
+++ usbrevue/codegen.py
@@ -2,19 +2,22 @@
 from usbrevue import *
 
 def packet_to_libusb_code(pack):
-    """Convert a captured USB packet into the libusb C code that would
-       replicate it. Currently assumes:
-           a) 'handle' is a valid libusb device handle
-           b) 'data' is a char array large enough for any incoming data
-           c) 'TIMEOUT' is a constant or int
-           d) 'err' and 'n_transferred' are ints"""
+    """
+    Convert a captured USB packet into the libusb C code that would
+    replicate it. Currently assumes:
+        a) 'handle' is a valid libusb device handle
+        b) 'data' is a char array large enough for any incoming data
+        c) 'TIMEOUT' is a constant or int
+        d) 'err' and 'n_transferred' are ints
+        e) 'handle_error()' is defined, and takes an int
+    """
     if pack.event_type != 'S':
         return ''
     if pack.is_control_xfer:
         data = 'data'
         if pack.setup.bmRequestTypeDirection == 'host_to_device':
             data = '"%s"' % ''.join(map(lambda x: "\\x%02x" % x, pack.data))
-        return 'err = libusb_control_transfer(handle, 0x%02x, 0x%x, 0x%x, 0x%x, %s, %s, TIMEOUT);\n' % (
+        return 'if ((err = libusb_control_transfer(handle, 0x%02x, 0x%x, 0x%x, 0x%x, %s, %s, TIMEOUT)) < 0)\n\thandle_error(err);\n' % (
                 pack.setup.bmRequestType,
                 pack.setup.bRequest,
                 pack.setup.wValue,
@@ -25,7 +28,7 @@ def packet_to_libusb_code(pack):
         data = 'data'
         if pack.epnum & 0x80 == 0:
             data = '"%s"' % ''.join(map(lambda x: "\\x%02x" % x, pack.data))
-        return 'err = libusb_%s_transfer(handle, 0x%02x, %s, %s, &n_transferred, TIMEOUT);\n' % (
+        return 'if ((err = libusb_%s_transfer(handle, 0x%02x, %s, %s, &n_transferred, TIMEOUT)) < 0)\n\thandle_error(err);\n' % (
                 'bulk' if pack.is_bulk_xfer else 'interrupt',
                 pack.epnum,
                 data,
