bd6a10e9f76065102c12708464f792ae84b39428
Wil Cooley <wcooley@pdx.edu>
2011-08-02 01:11:38 -0700

    Corrected slicing, negative offsets. Added docs, doctest, setitem.

--- subarray.py
+++ subarray.py
@@ -21,8 +21,47 @@
 #
 
 import array
+import logging
+
+from logging import debug
+
+#logging.basicConfig(level=logging.DEBUG)
+
+def _add_or_minus(base, addor):
+    """Add 'addor' to positive base; pass negative unchanged"""
+    if base >= 0:
+        base += addor
+    return base
+
+def _calc_offset(index, offset):
+    """Calculate offset, taking into account the possibility that 'index'
+    is a slice"""
+    if isinstance(index, slice):
+        start = index.start if index.start != None else 0
+        stop = index.stop if index.stop != None else 0
+        start = _add_or_minus(start, offset)
+        stop = _add_or_minus(stop, offset)
+        index = slice(start, stop, index.step)
+    else:
+        index = _add_or_minus(index, offset)
+    return index
 
 class subarray(object):
+    """The `subarray` class creates an wrapper for an array which allows
+    indexing from zero at a given offset, giving the appearance of a separate
+    array.
+
+    For example:
+    >>> parent = array.array('c', ['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+    >>> subarr = subarray(parent, 3)
+    >>> subarr[0] == 'd'
+    True
+
+    Note that currently only the beginning of offset; ending the subarray
+    shorter than the parent array is not currently supported.
+    >>> subarr[-1] == 'g'
+    True
+    """
 
     def __init__(self, parent_array=None, subarray_offset=0):
         self.parent_array = parent_array
@@ -31,5 +70,11 @@ class subarray(object):
         super(subarray, self).__init__()
 
     def __getitem__(self, index):
-        return self.parent_array[index + self.offset]
+        return self.parent_array[_calc_offset(index, self.offset)]
 
+    def __setitem__(self, index, val):
+        self.parent_array[_calc_offset(index, self.offset)] = val
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
